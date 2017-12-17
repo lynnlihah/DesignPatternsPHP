3dc6d886197c679097dc383a0c45d43e56677e1f
lizhufeng <lzhf09@126.com>
2017-12-17 22:20:58 +0800

    first commit

---
+++ Creational/AbstractFactory/Text.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace DesignPatterns\Creational\AbstractFactory;
+
+abstract class Text{
+
+	protected $text;
+
+	public function __construct(string $text){
+
+		$this->text = $text;
+	}
+
+}
